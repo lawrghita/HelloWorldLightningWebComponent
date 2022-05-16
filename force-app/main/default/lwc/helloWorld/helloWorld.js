3efaf78c1fe6f942753d8050da3c94beac107f90
Ghita Laurentiu <laurghita@yahoo.com>
2022-05-16 19:46:16 +0300

    Work

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-    greeting = 'World';
+    greeting = 'World1';
     changeHandler(event) {
       this.greeting = event.target.value;
     }
